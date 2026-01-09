0c3760e6f1f9a4e1936fe04472f934d8c990464a
Glory <glorysynala@gmail.com>
2026-01-09 22:41:45 +0530

    comments

--- Python/Api_basic_call.py
+++ Python/Api_basic_call.py
@@ -2,11 +2,13 @@ import requests
 
 url = "https://api.agify.io/?name=glory"
 
-response = requests.get(url)
+response = requests.get(url) #api call
 
-if response.status_code == 200:
-    data = response.json()
-    print(f"Name: {data['name']}")
+if response.status_code == 200:  #success chheck
+    data = response.json()        #convert json to dictionary
+    print(f"Name: {data['name']}") #access values
     print(f"Predicted Age: {data['age']}")
 else:
     print("Failed to fetch data from API")
+
+#simple rest api call to understand request
